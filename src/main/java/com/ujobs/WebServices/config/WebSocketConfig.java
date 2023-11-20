56ecd34a8d0b0d469be9b87c0b71df0bd5e7bd10
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2023-11-20 18:52:05 -0500

    Revert "cors v3"
    This reverts commit 4247d544514e6ddcdc6cc81669ae6db12019ca9f.

--- src/main/java/com/ujobs/WebServices/config/WebSocketConfig.java
+++ src/main/java/com/ujobs/WebServices/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://ujobs-frontend.netlify.app")
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
