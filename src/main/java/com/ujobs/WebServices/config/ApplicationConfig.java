e70a4f15b66a509e43f876bd22006999fd96a056
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2023-11-20 20:19:04 -0500

    fix
    fix

--- src/main/java/com/ujobs/WebServices/config/ApplicationConfig.java
+++ src/main/java/com/ujobs/WebServices/config/ApplicationConfig.java
@@ -54,7 +54,7 @@ public class ApplicationConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://ujobs-frontend.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
--- src/main/java/com/ujobs/WebServices/config/WebSocketConfig.java
+++ src/main/java/com/ujobs/WebServices/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200")
+                .setAllowedOrigins("https://ujobs-frontend.netlify.app")
                 .withSockJS();
     }
 
